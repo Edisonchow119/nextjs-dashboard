1c4bb81f06957bb18d4aa7d4240f6a1922ad587a
luka <christian.luka89@dontsp.am>
2024-02-22 12:00:21 +0800

    3 updates, Improve accessibility, Add authentication, Add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,8 +6,12 @@ import { fetchCardData } from '@/app/lib/data'
 import { Suspense } from 'react'
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons'
 import CardWrapper from '@/app/ui/dashboard/cards'
+import { Metadata } from 'next';
 
- 
+export const metadata: Metadata = {
+  title: 'Home'
+}
+
 export default async function Page() {
   return (
     <main>
